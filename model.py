683f8e9b4b6026850963bdeecd2c1bc182c29010
sunshuofeng <45551402+sunshuofeng@users.noreply.github.com>
2022-12-24 11:17:42 +0800

    Update model.py

--- model.py
+++ model.py
@@ -7,15 +7,74 @@ import torch.nn.functional as F
 from torch.nn import init
 
 from utils.model_utils import GeMP, resnet50
+class Uncertain(nn.Module):
+    def __init__(self,inchannel) -> None:
+        super().__init__()
+        self.conv=nn.Sequential(
+            nn.Conv2d(inchannel,int(inchannel/4),kernel_size=3,padding=1,stride=1),
+            nn.BatchNorm2d(int(inchannel/4)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(int(inchannel/4),1,kernel_size=1),
+           
+          
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+def get_uncertain_loss(feat_q,feat_k,mask_q,mask_k,uncertain_q,uncertain_k):
+    ##分两个来
+    batch_size, fdim, h, w = feat_q.shape
+    feat_q = feat_q.view(batch_size, fdim, -1).detach().contiguous()
+    feat_k = feat_k.view(batch_size, fdim, -1).detach().contiguous()
+    uncertain_q=torch.exp(uncertain_q.view(batch_size,-1).contiguous())
+    uncertain_k=torch.exp(uncertain_k.view(batch_size,-1).contiguous())
+    mask_q=mask_q.view(batch_size,-1).detach().contiguous()
+    mask_k=mask_k.view(batch_size,-1).detach().contiguous()
+    loss=0
+
+
+    ##先计算q的每个特征节点对应k的哪个节点
+    feat_sim=torch.bmm(F.normalize(feat_q, dim=1).permute(0,2,1).contiguous(), F.normalize(feat_k, dim=1))
+    index=torch.argmax(feat_sim,dim=-1)
+    feat_k_to_q=feat_k.gather(dim=-1,index=index.unsqueeze(1).repeat(1,fdim,1)).contiguous()
+    uncertain_k_to_q=uncertain_k.gather(dim=-1,index=index)
+    u=uncertain_q+uncertain_k_to_q
+    sim_q=torch.sum(torch.square(feat_q-feat_k_to_q),dim=1)
+    sim_q=sim_q*mask_q
+    # label=torch.arange(feat_q.shape[-1]).unsqueeze(0).repeat(batch_size,1).to(feat_q.device)    
+    loss=loss+torch.mean(sim_q/(1e-10+u)+torch.log(u))/2
+    
+
+    feat_sim=torch.bmm(F.normalize(feat_k, dim=1).permute(0,2,1).contiguous(), F.normalize(feat_q, dim=1))
+    index=torch.argmax(feat_sim,dim=-1)
+    feat_q_to_k=feat_q.gather(dim=-1,index=index.unsqueeze(1).repeat(1,fdim,1)).contiguous()
+    uncertain_q_to_k=uncertain_q.gather(dim=-1,index=index)
+    u=uncertain_q_to_k+uncertain_k
+    sim_k=torch.square(feat_k-feat_q_to_k)
+    sim_k=torch.sum(sim_k,dim=1)
+    sim_k=sim_k*mask_k
+    loss=loss+torch.mean(sim_k/(1e-10+u)+torch.log(u))/2
+
+
+    return loss
+
+
+# def Uncertain_aware_loss(feat_fusion,)
+
 
 
 class CMAlign(nn.Module):
-    def __init__(self, batch_size=8, num_pos=4, temperature=50):
+    def __init__(self, batch_size=8, num_pos=4, temperature=50,dim=2048):
         super(CMAlign, self).__init__()
         self.batch_size = batch_size
         self.num_pos = num_pos
         self.criterion = nn.TripletMarginLoss(margin=0.3, p=2.0, reduce=False)
         self.temperature = temperature
+        self.q=nn.Conv2d(dim,int(dim/4),kernel_size=3,padding=1)
+        self.k=nn.Conv2d(dim,int(dim/4),kernel_size=3,padding=1)
+        
 
     def _random_pairs(self):
         batch_size = self.batch_size
@@ -55,12 +114,28 @@ class CMAlign(nn.Module):
 
         return {'pos': np.concatenate((pos_v, pos_t)), 'neg': np.concatenate((neg_v, neg_t))}
 
-    def feature_similarity(self, feat_q, feat_k):
+    def feature_similarity_uncertain(self, feat_q, feat_k,uncertain_q,uncertain_k):
         batch_size, fdim, h, w = feat_q.shape
+        
         feat_q = feat_q.view(batch_size, fdim, -1)
         feat_k = feat_k.view(batch_size, fdim, -1)
+        uncertain_q=torch.exp(uncertain_q.view(batch_size,1,-1).contiguous()).detach()
+        uncertain_k=torch.exp(uncertain_k.view(batch_size,-1,1).contiguous()).detach()
+        u=uncertain_q+uncertain_k
+        feature_sim = torch.bmm(F.normalize(feat_q, dim=1).permute(0,2,1).contiguous(), F.normalize(feat_k, dim=1))
+        feature_sim=feature_sim/(1e-10+u)
+        return feature_sim
 
-        feature_sim = torch.bmm(F.normalize(feat_q, dim=1).permute(0,2,1), F.normalize(feat_k, dim=1))
+    def feature_similarity(self, feat_q, feat_k):
+        batch_size, fdim, h, w = feat_q.shape
+        feat_q=self.q(feat_q)
+        feat_k=self.k(feat_k)
+
+        feat_q = feat_q.view(batch_size, int(fdim/4), -1)
+        feat_k = feat_k.view(batch_size, int(fdim/4), -1)
+     
+        feature_sim = torch.bmm(F.normalize(feat_q, dim=1).permute(0,2,1).contiguous(), F.normalize(feat_k, dim=1))
+
         return feature_sim
 
     def matching_probability(self, feature_sim):
@@ -95,8 +170,9 @@ class CMAlign(nn.Module):
         batch_size, mdim, h, w = mask_q.shape
         mask_q = mask_q.view(batch_size, -1, 1)
         mask_k = mask_k.view(batch_size, -1, 1)
+        ##首先 match_pr与mask_k的运算结果代表着融合节点是有意义的
         comask = mask_q * torch.bmm(matching_pr, mask_k)
-        
+     
         comask = comask.view(batch_size, -1)
         comask -= comask.min(dim=-1, keepdim=True)[0]
         comask /= comask.max(dim=-1, keepdim=True)[0] + 1e-12
@@ -104,8 +180,9 @@ class CMAlign(nn.Module):
         
         return comask.detach()
 
-    def forward(self, feat_v, feat_t):
+    def forward(self, feat_v, feat_t,uncertain_v,uncertain_t):
         feat = torch.cat([feat_v, feat_t], dim=0)
+        uncertain=torch.cat([uncertain_v,uncertain_t],dim=0)
         mask = self.compute_mask(feat)
         batch_size, fdim, h, w = feat.shape
 
@@ -114,11 +191,17 @@ class CMAlign(nn.Module):
 
         # positive
         feat_target_pos = feat[pos_idx]
+        # uncertain_loss=get_uncertain_loss(feat,feat_target_pos,mask,mask[pos_idx],uncertain,uncertain[pos_idx])
+        
         feature_sim = self.feature_similarity(feat, feat_target_pos)
+        ##B,N,N 其中N为特征节点数量,意思是feat的N个节点与target pos的N个解点的相似度
         matching_pr = self.matching_probability(feature_sim)
         
+        ##计算对于feat来说 feat_target_pos重建后哪些节点有意义,这个不用管
         comask_pos = self.compute_comask(matching_pr, mask, mask[pos_idx])
+
         feat_warp_pos = self.soft_warping(matching_pr, feat_target_pos)
+
         feat_recon_pos = self.reconstruct(mask, feat_warp_pos, feat)
 
         # negative
@@ -130,7 +213,7 @@ class CMAlign(nn.Module):
         feat_recon_neg = self.reconstruct(mask, feat_warp, feat)
 
         loss = torch.mean(comask_pos * self.criterion(feat, feat_recon_pos, feat_recon_neg))
-
+        loss=loss
         return {'feat': feat_recon_pos, 'loss': loss}
 
 # #####################################################################
@@ -221,8 +304,12 @@ class embed_net(nn.Module):
         self.classifier.apply(weights_init_classifier)
         self.pool = GeMP()
 
-        self.cmalign = CMAlign(args.batch_size, args.num_pos)
-
+        self.cmalign1 = CMAlign(args.batch_size, args.num_pos,dim=1024)
+        self.cmalign2 = CMAlign(args.batch_size, args.num_pos,dim=2048)
+        self.uncertain1=Uncertain(1024)
+        self.uncertain2=Uncertain(2048)
+        self.uncertain1.apply(weights_init_kaiming)
+        self.uncertain2.apply(weights_init_kaiming)
 
     def forward(self, x_v, x_t, modal=0, use_cmalign=True):
         if modal == 0:
@@ -240,7 +327,8 @@ class embed_net(nn.Module):
             ### layer3
             feat3 = feat['feat3']
             batch_size, fdim, h, w = feat3.shape
-            out3 = self.cmalign(feat3[:batch_size//2], feat3[batch_size//2:])
+            uncertain=self.uncertain1(feat3)
+            out3 = self.cmalign1(feat3[:batch_size//2], feat3[batch_size//2:],uncertain[:batch_size//2],uncertain[batch_size//2:])
 
             feat3_recon = self.base_resnet.base.layer4(out3['feat'])
             feat3_recon_p = self.pool(feat3_recon)
@@ -250,8 +338,9 @@ class embed_net(nn.Module):
             feat4 = feat['feat4']
             feat4_p = self.pool(feat4)
             batch_size, fdim, h, w = feat4.shape
-            out4 = self.cmalign(feat4[:batch_size//2], feat4[batch_size//2:])
-
+            uncertain=self.uncertain2(feat4)
+            out4 = self.cmalign2(feat4[:batch_size//2], feat4[batch_size//2:],uncertain[:batch_size//2],uncertain[batch_size//2:])
+            
             feat4_recon = out4['feat']
             feat4_recon_p = self.pool(feat4_recon)
             cls_ic_layer4 = self.classifier(self.bottleneck(feat4_recon_p))
